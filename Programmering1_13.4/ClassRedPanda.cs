74a6b284882596b131e206442a0b18b471aa4f77
Monica Björk <m.bjork95@gmail.com>
2018-05-10 20:26:22 +0200

    Done!

--- Programmering1_13.4/ClassRedPanda.cs
+++ Programmering1_13.4/ClassRedPanda.cs
@@ -7,16 +7,33 @@ using System.Threading.Tasks;
 namespace Programmering1_13._4 {
     class RedPanda {
         //      SAME FOR ALL
-        public static int eyes = 2;
-        public static int legs = 4;
-        public static int tail = 1;
+        private static int eyes = 2;
+        private static int legs = 4;
+        private static int tail = 1;
 
         //      UNIQUE FOR ALL
-        public static string name;
-        public static int age;
+        private string name;
+        public string Name {
+            get {
+                return name;
+            }
+            set {
+                name = value;
+            }
+        }
+
+        private int age;
+        public int Age {
+            get {
+                return age;
+            }
+            set {
+                age = value;
+            }
+        }
 
         //      STUFF THEY DO
-        public static void Sleep() {    // No need to mention "name" as an agument here as they're in the same class.
+        public static void Sleep(string name) {    // Need to mention "name" as an agument here since it's private.
             Console.WriteLine("sssh, {0} sover", name);
             return;
         }
--- Programmering1_13.4/Program.cs
+++ Programmering1_13.4/Program.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 namespace Programmering1_13._4 {
     class Program {
         static void Main(string[] args) {
-            RedPanda myPanda = new RedPanda();
-            RedPanda.name = "Tails";
-            RedPanda.age = 23;
+            RedPanda redPanda = new RedPanda();
+            redPanda.Name = "Tails";
+            redPanda.Age = 23;
 
-            Console.WriteLine("Denna kattbjörnen heter {0} och är {1}år gammal.", RedPanda.name, RedPanda.age);
+            Console.WriteLine("Denna kattbjörnen heter {0} och är {1}år gammal.", redPanda.Name, redPanda.Age);
             Console.ReadKey();
 
-            RedPanda.Sleep();
+            RedPanda.Sleep(redPanda.Name);
             Console.ReadKey();
         }
     }
