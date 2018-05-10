bcf8863883945b66ddd5a548ff9efba6895cb341
Monica Björk <m.bjork95@gmail.com>
2018-05-10 12:32:54 +0200

    Add project files.

---
+++ Programmering1_13.4/ClassRedPanda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_13._4 {
+    class RedPanda {
+        //      SAME FOR ALL
+        public static int eyes = 2;
+        public static int legs = 4;
+        public static int tail = 1;
+
+        //      UNIQUE FOR ALL
+        public static string name;
+        public static int age;
+
+        //      STUFF THEY DO
+        public static void Sleep() {    // No need to mention "name" as an agument here as they're in the same class.
+            Console.WriteLine("sssh, {0} sover", name);
+            return;
+        }
+    }
+}
---
+++ Programmering1_13.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_13._4 {
+    class Program {
+        static void Main(string[] args) {
+            RedPanda myPanda = new RedPanda();
+            RedPanda.name = "Tails";
+            RedPanda.age = 23;
+
+            Console.WriteLine("Denna kattbjörnen heter {0} och är {1}år gammal.", RedPanda.name, RedPanda.age);
+            Console.ReadKey();
+
+            RedPanda.Sleep();
+            Console.ReadKey();
+        }
+    }
+}
